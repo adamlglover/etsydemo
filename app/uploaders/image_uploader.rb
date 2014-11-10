d94c0f4c8636b40df76affed4f29b6bb2d8f4c00
adamlglover <adamlglover85@gmail.com>
2014-11-10 22:00:53 +0000

    etsy look

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,12 +4,15 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  # storage :file
+  if Rails.env.production?
   storage :dropbox
-  # storage :fog
+  else
+     storage :file
+  end
+    # storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -26,16 +29,20 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  # process :scale => [200, 300]
+    process :resize_to_fill => [600, 900]
   #
   # def scale(width, height)
   #   # do something
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process :resize_to_fit => [50, 50]
-  # end
+    version :medium do
+  process :resize_to_fill => [800, 800]
+  end
+    
+    version :thumb do
+  process :resize_to_fill => [200, 200]
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
